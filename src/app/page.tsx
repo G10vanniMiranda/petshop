d1d725137794c803b6f3eaa759df479a84895a49
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-03-21 23:12:50 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+
+      <About />
     </div>
   );
 }
